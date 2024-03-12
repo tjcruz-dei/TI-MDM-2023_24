c1bb93dd64a71ff154a0ccf0203dbb4f35994b31
Tiago Cruz <tjcruz@dei.uc.pt>
2024-03-12 23:34:10 +0000

    Simplification Homework Challenge

--- PL4-Serial-communications/Exercises/Homework Challenge 1/arducontroller/arducontroller.ino
+++ PL4-Serial-communications/Exercises/Homework Challenge 1/arducontroller/arducontroller.ino
@@ -1,27 +1,15 @@
-/**
- * Tecnologias de Interface
- * Universidade de Coimbra
- * MSc in Design and Multimedia
- *
- * Author: Tiago Cruz
- * Week 4
- * Homework challenge 1
- *
- * @since:   02–03–2024
- */
- 
 long timestamp;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(8,INPUT_PULLUP);
-  pinMode(9,INPUT_PULLUP);
+  pinMode(6,INPUT_PULLUP);
+  pinMode(7,INPUT_PULLUP);
 }
 
 void loop() {
   if (millis()-timestamp>50) {
-    if (!digitalRead(8)) Serial.write('R');
-    if (!digitalRead(9)) Serial.write('L');
+    if (!digitalRead(6)) Serial.write('R');
+    if (!digitalRead(7)) Serial.write('L');
   }
 }
